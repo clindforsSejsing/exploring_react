873bcfebdbf922e955bb0bbf1e5bd835dc6545f2
CLS <clindforsSejsing@gmail.com>
2022-02-08 13:54:41 +0100

    changed files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from './components/ExpenseItem.js';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+      <p>This is also visible!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+
+    return (
+        <div>
+            <div>Date</div>
+            <div>
+                <h2>title</h2>
+            </div>
+            <div>Amount</div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
