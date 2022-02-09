dd026c0a74bf40b07113a9e3e7969a25211ff606
CLS <clindforsSejsing@gmail.com>
2022-02-09 11:38:26 +0100

    added an event

--- my-app/src/components/Expenses/ExpenseItem.js
+++ my-app/src/components/Expenses/ExpenseItem.js
@@ -2,13 +2,20 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
+
 const ExpenseItem = (props) => {
+
+    const clickHandler = () => {
+        console.log('Click-edi-clicked');
+    }; //when usig function it does leave out the brackets, otherwise it will execute at the same time as the jsx-code below
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
+                <button onClick={clickHandler}>Change Title</button>
+                {/* <button onClick={() => { console.log('clicked') }}>Change Title</button> */}
             </div>
         </Card>
     );
